7556e9148f05d14777813858af564280b617e447
Phil Picton <phil.picton@digitickets.co.uk>
2023-06-26 19:32:38 +0100

    add nightowl 🦉

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  -- add night-owl
+  { "oxfist/night-owl.nvim" },
+
+  -- Configure LazyVim to load nightowl
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "night-owl",
+    },
+  }
+}
