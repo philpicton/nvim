f01954a1fe767ef1502420f4062a9cd2a3abdb0f
Phil Picton <philpicton@gmail.com>
2023-07-06 23:35:15 +0100

    remove the lsp config, call volar from init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("lspconfig").volar.setup({
+  filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+})
+require("lspconfig").tailwindcss.setup({})
