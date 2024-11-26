49c97eeeda50afb54bb53320d9db9677fb355e7e
Phil Picton <phil.picton@digitickets.co.uk>
2024-11-26 14:45:05 +0000

    snack dashboard customisation for work. updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,7 +6,3 @@ vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
-if vim.g.neovide then
-  -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "FiraCode Nerd Font,Noto_Color_Emoji"
-end
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,3 +1,64 @@
+-- Custom header function for my digitickets machine
+local function getDTHeader()
+  local hour = tonumber(os.date("%H"))
+
+  local greeting = ""
+  if hour >= 0 and hour <= 11 then
+    greeting = "Good morning Phil you gorgeous muffin"
+  elseif hour >= 12 and hour <= 17 then
+    greeting = "Good afternoon Phil you absolute legend"
+  elseif tonumber(hour) >= 18 and tonumber(hour) <= 23 then
+    greeting = "Good evening Phil what you doing working? Turn it off mate"
+  end
+
+  local header = [[
+            xxx                                                              
+          xxxxxxx                                                            
+        000Qnxxxxxx                                                          
+      0000000Xxxxxxxr                                                        
+    0000000JXXXzxxxxxrx        ____  _       _ _____ _      _        _       
+  0000000mwwmYXXXzxxxxr/~     |  _ \(_) __ _(_)_   _(_) ___| | _____| |_ ___ 
+0000000Zwwwwp YXz)1(r|~~~~~   | | | | |/ _` | | | | | |/ __| |/ / _ \ __/ __|
+00000JYOwm000 11111}~~~~~~~   | |_| | | (_| | | | | | | (__|   <  __/ |_\__ \
+  0JXXXXYQQQQJ)11{~~~~~~~     |____/|_|\__, |_| |_| |_|\___|_|\_\___|\__|___/
+    YXXXXXYQQQQc~~~~~~~                |___/                                 
+      XXXXXXYJ_~~~~~~                                                        
+        XXXXXXc_~~~                                                          
+          YYXXXXz                                                            
+            YXY                                                              
+]]
+  header = header .. "\n" .. greeting
+  return header
+end
+
+-- Are we on my DT mbp?
+if vim.fn.hostname() == "Phils-MacBook-Pro.local" then
+  return {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = getDTHeader(),
+        -- stylua: ignore
+        ---@type snacks.dashboard.Item[]
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = " ", key = "v", desc = "DT Open vue3 folder", action = ":lua Snacks.dashboard.pick('files', {cwd = '~/dt/app/ui/backoffice-vue3'})" },
+          { icon = " ", key = "a", desc = "DT Open app folder", action = ":lua Snacks.dashboard.pick('files', {cwd = '~/dt/app'})" },
+          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        },
+        },
+      },
+    },
+  }
+end
+-- If not here's the default snacks setup
 return {
   "folke/snacks.nvim",
   opts = {
@@ -32,7 +93,6 @@ return {
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-          { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
           { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
